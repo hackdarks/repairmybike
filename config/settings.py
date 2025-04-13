bd208bcbdea470ffb527e7392cc7c9132a81da07
wolf <nrdyw01f@gmail.com>
2025-04-14 04:57:26 +0530

    confic setting

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-DJANGO_SECRET_KEY = 'django-insecure-f1d3d51b88$qg3c$zfn%^z$rfw@esiym5s#e*5tm$z3^+r^xqn'
+SECRET_KEY = 'django-insecure-f1d3d51b88$qg3c$zfn%^z$rfw@esiym5s#e*5tm$z3^+r^xqn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
